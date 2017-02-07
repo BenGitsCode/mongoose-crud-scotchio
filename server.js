aeac02eec945950b23e68f90b0b397f18bb0bf4c
Ben Adamski <benjadamski@gmail.com>
2017-02-07 09:17:53 -0500

    1. Fix vim induced errors in server.js
    - Someday we will be unstoppable vim. That day is not today.
    - Mistyped a const assignment that broke the file. Would have caught it
      in Atom.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // grab our dependencies
-const express - require('express'),
-      app = express(),
-      port = process.env.PORT || 8080;
-//configure application
-//
-//set routes
+// grab our dependencies
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8080;
+
+// configure our application
+
+// set the routes
 app.get('/', (req, res) => {
-	res.send('Hello, I am the app!');
+  res.send('Hello, I am the app!');
 });
-//start our server
+
+// start our server
 app.listen(port, () => {
-	console.log(`App listening on http://localhost:${port}`)
+  console.log(`App listening on http://localhost:${port}`);
 });
