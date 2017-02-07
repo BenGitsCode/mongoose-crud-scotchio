2075c900a1ed45ca0cd8d997d5359c9e55084e0c
Ben Adamski <benjadamski@gmail.com>
2017-02-07 12:09:21 -0500

    3. Templating
    - Set up basic layout template using ejs.
    - Route to layout.
    - Basic home page view
    - Events section to created, to be built in next step.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // grab our dependencies
-// grab our dependencies
 const express = require('express'),
   app = express(),
-  port = process.env.PORT || 8080;
+  expressLayouts = require('express-ejs-layouts'),
+  port = process.env.PORT || 8080,
+  path = require('path');
+
 
 // configure our application
+// tell express where to look for static assets
+app.use(express.static(__dirname + '/public'));
+
+// set ejs as our templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 
 // set the routes
 app.use(require('./app/routes'));
