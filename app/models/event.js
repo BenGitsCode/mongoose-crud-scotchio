375f9d58f40bbfc6ecff41e4f568ab82d60f7c58
Ben Adamski <benjadamski@gmail.com>
2017-02-13 20:32:17 -0500

    8. Add slugify function I wrote
    -  Just kidding, I didn't write that. Thanks internet! :grin:

--- app/models/event.js
+++ app/models/event.js
@@ -25,3 +25,13 @@ const mongoose = require('mongoose'),
 
     //export the model
     module.exports = eventModel;
+
+    // function to slugify a name
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
