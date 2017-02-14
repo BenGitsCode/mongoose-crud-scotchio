85f7dcedec5abb28c43fc54e4035618dfff8b13d
Ben Adamski <benjadamski@gmail.com>
2017-02-13 20:22:16 -0500

    8. Call middleware to slugify slugs based on names
    -  Call using function form NOT fat arrow form, as it would eff up
    the reference to the object.

--- app/models/event.js
+++ app/models/event.js
@@ -10,6 +10,16 @@ const mongoose = require('mongoose'),
       },
     });
 
+    // middleware ================
+    // make sure that the slug is created from the name
+    // NEEDS to be function form instead of => is because
+    // we need to reference this to create the slug, fat arrow
+    // would alter the way the object is referenced.
+    eventSchema.pre('save', function(next) {
+      this.slug = slugify(this.name);
+      next();
+    });
+
     // Create model
     const eventModel = mongoose.model('Event', eventSchema);
 
