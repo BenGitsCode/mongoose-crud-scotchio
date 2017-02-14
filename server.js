c8ba42ab90b3e592b54d9842d3220f8ff12c8e78
Ben Adamski <benjadamski@gmail.com>
2017-02-13 20:05:32 -0500

    6. Connect to mlab database w/ mongoDB URI
    -  Sloppy, but effective.
    
    -  Move into env on next commit.

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-// grab our dependencies
+// grab the dependencies
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 8080,
   expressLayouts = require('express-ejs-layouts');
+  mongoose = require('mongoose');
 
-// configure our application
+// configure the application
 // tell express where to look for static assets
 app.use(express.static(__dirname + '/public'));
 
-// set ejs as our templating engine
+// set ejs as the templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+//Connect to mlab database
+mongoose.connect('mongodb://bengitscode:deovolente@ds059135.mlab.com:59135/heroku_m8dvgp9b');
 
 // set the routes
 app.use(require('./app/routes'));
 
-// start our server
+// start the server
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
